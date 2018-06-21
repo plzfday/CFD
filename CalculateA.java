66515ff2df44af8edfcb1bf4ffc90af32e003938
plzfday <qkrehddus01@gmail.com>
2018-06-21 19:31:54 +0900

    correction

--- CalculateA.java
+++
@@ -1,9 +0,0 @@
-package dev;
-
-abstract public class CalculateA {
-    // �굹以묒뿉 calculate, commander�뒗 �뿬�윭 援곕뜲�뿉�꽌 �벝 �닔 �엳�쓣 寃� 媛숈븘�꽌 abstract class濡� �뵲濡� 鍮쇰넃�쓬.
-    abstract public void Commander();
-    abstract public double Calculate();
-}
-
-
--- StackI.java
+++
@@ -1,9 +0,0 @@
-package dev;
-
-public interface StackI {
-    boolean is_empty();
-    long size();
-    Object top();
-    void push(Object data);
-    Object pop();
-}
