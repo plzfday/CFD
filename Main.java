a4b88b3c0d870dbdc6672ecdaa030af15acae8a4
Dongyeon Park <qkrehddus01@naver.com>
2018-06-21 14:47:26 +0900

    Error correction - to Error#1

---
+++ Main.java
@@ -0,0 +1,43 @@
+package dev;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void Menu() {
+        System.out.println("===============\n   Calculator\n===============");
+        System.out.println("1: Standard");
+//        System.out.println("---------- Yet Developed ----------");
+//        System.out.println("2: Scientific");
+//        System.out.println("3: Data");
+//        System.out.println("* Converter");
+//        System.out.println("4: Length");
+//        System.out.println("5: Weight and mass");
+//        System.out.println("6: Temperature");
+//        System.out.println("7: Data");
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        Menu();
+        int menu = reader.nextInt();
+        switch (menu) {
+            case 1:
+                String s = reader.nextLine();
+                s=reader.nextLine();
+                StandardCalculator sc = new StandardCalculator(s);
+                sc.Commander();
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            default:
+                System.out.println("죄송합니다. 아직 구현 중 입니다 ㅠㅠ");
+                break;
+        }
+        reader.close();
+    }
+}
